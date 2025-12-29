ca4ec4c26ec3dd6773323df9e3284eec98f6adbf
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-29 23:18:07 +0500

    Initial commit

--- app.js
+++ app.js
@@ -18,3 +18,22 @@ console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", 
 console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
 console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
 console.log("Total: ", total);
+
+const rosePrice = 8;
+const lilyPrice = 10;
+const tulipPrice = 2;
+
+let numberOfRoses = 70;
+let numberOfLilies = 50;
+let numberOfTulips = 120;
+
+let rosesValue = rosePrice * numberOfRoses;
+let liliesValue = lilyPrice * numberOfLilies;
+let tulipsValue = tulipPrice * numberOfTulips;
+
+let total = rosesValue + liliesValue + tulipsValue;
+
+console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+console.log("Total: ", total);
