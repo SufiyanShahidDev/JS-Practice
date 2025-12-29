1738da6f0160461bb50400aa182a09deb9ce90a2
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-30 00:02:17 +0500

    Initial commit

--- app.js
+++ app.js
@@ -266,3 +266,69 @@ switch (gate) {
 if (win) {
     alert("Winner!");
 }
+
+let movies = [];
+while (true) {
+    let title = prompt("Enter movie title");
+    let rating = prompt("Enter movie rating (imdb)");
+
+    if (title === null || rating === null) {
+        break
+    } else {
+        movies.push({
+            title: title,
+            rating: Number(rating)
+        });
+    }
+}
+
+console.log("All with ratings under 7:");
+for (movie of movies) {
+    if (movie.rating < 7) {
+        console.log(`${movie.title} (${movie.rating})`);
+    }
+}
+
+console.log("All with ratings over 7:");
+for (movie of movies) {
+    if (movie.rating >= 7) {
+        console.log(`${movie.title} (${movie.rating})`);
+        break;
+    }
+}
+
+while (true) {
+    let firstNumber = prompt("Enter first number");
+    let secondNumber = prompt("Enter second number");
+    let operand = prompt("Enter operand (+, -, * or /)");
+    let result;
+
+    if (firstNumber === "Q" || secondNumber === "Q" || operand === "Q") {
+        break;
+    }
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+        switch (operand) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                result = "Error: unknown operand";
+        }
+    } else {
+        result = "Error: at least one of the entered values is not a number";
+    }
+    alert(result);
+}
