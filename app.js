ce1a7b275368237827c3016d114a084920e4a2ee
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-29 23:35:45 +0500

    Initial commit

--- app.js
+++ app.js
@@ -237,3 +237,32 @@ for (let city of cities) {
         console.log(`${city.name} (${city.population})`);
     }
 }
+
+let gate = prompt("Choose gate: a, b, or c");
+let win = false;
+switch (gate) {
+    case "a":
+    case "A":
+    case 1:
+    case "1":
+        alert("Gate A: empty");
+        break;
+    case "b":
+    case "B":
+    case 2:
+    case "2":
+        alert("Gate B: main prize");
+        win = true;
+        break;
+    case "c":
+    case "C":
+    case 3:
+    case "3":
+        alert("Gate C: empty");
+        break;
+    default:
+        alert("No gate " + String(gate));
+}
+if (win) {
+    alert("Winner!");
+}
