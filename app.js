753ea293bc5c29bed7a3d1272d2ccbd97b90cbb6
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-29 23:26:30 +0500

    Initial commit

--- app.js
+++ app.js
@@ -163,3 +163,25 @@ if(number > 90 && number < 110) {
 } else {
     alert("Miss");
 }
+
+let number = prompt("Enter a random number");
+let message = (number > 90 && number < 110) ? "Bingo!": "Miss";
+alert(message);
+
+let firstNumber = Number(prompt("Enter first number"));
+let secondNumber = Number(prompt("Enter second number"));
+let operand = prompt("Enter operand (+, -, * or /)");
+let result;
+
+if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+    switch (operand) {
+        case "+": result = firstNumber + secondNumber; break;
+        case "-": result = firstNumber - secondNumber; break;
+        case "*": result = firstNumber * secondNumber; break;
+        case "/": result = firstNumber / secondNumber; break;
+        default: result = "Error: unknown operand";
+    }
+} else {
+    result = "Error: at least one of the entered values is not a number";
+}
+alert(result);
