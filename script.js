b7a018ff6fefcd1c5893e48458b49b46d77ae1d0
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-03 16:30:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let a = window;
+
+console.log(a);
+
+let b = window.location;
+
+console.log(b);
+
+window.setTimeout(function () {
+    window.location.href = "https://github.com/"
+}, 4000);
+
+
+window.setTimeout(function () {
+    // window.location.href = "https://github.com/"
+    window.open("https://github.com/","_blank")
+
+
+}, 3000);
+
+let id = window.setInterval(function () {
+    // window.location.href = "https://github.com/"
+    window.open("https://github.com/","_blank")
+
+
+}, 5000);
+
+clearInterval(id)
