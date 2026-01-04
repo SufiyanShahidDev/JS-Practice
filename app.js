61466e8fcbb18627b269810ad153c2a4dd608627
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-04 20:39:14 +0500

    Initial commit

--- app.js
+++ app.js
@@ -518,7 +518,14 @@ function fillCity() {
         case "60608":
             cityName = "Chicago";
             break;
-        // case "68114":
-        //     cityName = "Omaha";
-        //     break;
+        case "68114":
+            cityName = "Omaha";
+            break;
+//         case "53212":
+//             cityName = "Milwaukee";
+//     }
+
+//     document.getElementById("city").value = cityName;
+
+// }
 
