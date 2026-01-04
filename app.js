61e298df7b8b3c9ebefd52ac8470cbec0138a025
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-04 21:00:32 +0500

    Initial commit

--- app.js
+++ app.js
@@ -553,6 +553,6 @@ for (var i = 0; i < par.length; i++) {
     par[i].style.fontFamily = "Verdana, Geneva, sans-serif";
 }
 
-// let childrens = document.childNodes[1].childNodes[2].childNodes[1];
+let childrens = document.childNodes[1].childNodes[2].childNodes[1];
 
-// console.log(childrens);
+console.log(childrens);
