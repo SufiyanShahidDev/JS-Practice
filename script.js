3ac1956dc840c1aa7f4ce6f9902692284c3c16bd
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-08 15:54:38 +0500

    Initial Commit

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 
-function showDateTime() {
-    let now = new Date();
+// function showDateTime() {
+//     let now = new Date();
 
-    let days = [
-        "Sun",
-        "Mon",
-        "Tues",
-        "Wed",
-        "Thur",
-        "Fri",
-        "Sat"
-    ];
+//     let days = [
+//         "Sun",
+//         "Mon",
+//         "Tues",
+//         "Wed",
+//         "Thur",
+//         "Fri",
+//         "Sat"
+//     ];
 
-    let dayName = days[now.getDay()];
+//     let dayName = days[now.getDay()];
 
-    let months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "June",
-        "July",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ];
+//     let months = [
+//         "Jan",
+//         "Feb",
+//         "Mar",
+//         "Apr",
+//         "May",
+//         "June",
+//         "July",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec"
+//     ];
 
-    monName = months[now.getMonth()];
+//     monName = months[now.getMonth()];
 
-    currentDate = now.getDate();
-    currentYear = now.getFullYear();
+//     currentDate = now.getDate();
+//     currentYear = now.getFullYear();
 
-    document.getElementById("date").innerHTML = `${currentDate} ${monName}, ${dayName} ${currentYear}`;
+//     document.getElementById("date").innerHTML = `${currentDate} ${monName}, ${dayName} ${currentYear}`;
 
-    let timeAm = "AM";
-    let timePm = "PM";
-    let hour = now.getHours();
-    let hours = now.getHours() % 12;
-    let min = now.getMinutes();
-    let sec = now.getSeconds()
+//     let timeAm = "AM";
+//     let timePm = "PM";
+//     let hour = now.getHours();
+//     let hours = now.getHours() % 12;
+//     let min = now.getMinutes();
+//     let sec = now.getSeconds();
 
-    document.getElementById("time").innerHTML = `${hours}:${min}:${sec}`
+//     document.getElementById("time").innerHTML = `${hours}:${min}:${sec}`;
 
-    if(hour >= 12) {
-    document.getElementById("time").innerHTML = `${hours}:${min}:${sec} ${timePm}`
-    }else{
-    document.getElementById("time").innerHTML = `${hours}:${min}:${sec} ${timeAm}`
-    }
-}
+//     if(hour >= 12) {
+//     document.getElementById("time").innerHTML = `${hours}:${min}:${sec} ${timePm}`;
+//     }else{
+//     document.getElementById("time").innerHTML = `${hours}:${min}:${sec} ${timeAm}`;
+//     };
+// };
 
-showDateTime()
+// showDateTime();
 
-setInterval(showDateTime, 1000)
+// setInterval(showDateTime, 1000);
